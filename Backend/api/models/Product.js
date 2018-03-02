f5077ca7af287922bf4b241f43447b86286f0a1c
salmaosamaelkholy <salmaosama@outlook.com>
2018-03-02 22:48:34 +0200

    Commiting my files

---
+++ Backend/api/models/Product.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+
+var productSchema = mongoose.Schema({
+  
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt:{
+    type: Date,
+  },
+  sellerName:{
+
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+
+  },
+});
+
+mongoose.model('Product', productSchema);
+
+
